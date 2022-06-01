c6abe9e2c4cb332d91beff0831c137bf5db3bfb9
VenkataSriram S <68543427+cybersriram@users.noreply.github.com>
2022-06-01 10:45:57 +0530

    Add files via upload

--- IP Assignment 4/cal.php
+++ IP Assignment 4/cal.php
@@ -2,6 +2,20 @@
 $date=date_create($_POST["date"]);
 $date2 = date_create(date("m/d/Y"));
 $interval = date_diff($date,$date2);
+$conn = mysqli_connect(
+    "localhost", 
+    "root", 
+    '',
+    'ip4');
+if(!$conn){
+    echo 'Connection error: ' . mysqli_connect_error();
+}
+$conn-> autocommit(TRUE);
+$res = $interval->format('%y').' years'.$interval->format('%m').' months';
+$sql = "insert into details values('".$_POST['name']."','".$res."')";
+if ($conn->query($sql) === TRUE){
+    print "rec addded";
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -13,7 +27,7 @@ $interval = date_diff($date,$date2);
 </head>
 <body>
     <div style="text-align:center;margin-top:140px">
-    <h1> Hello </h1>
+    <h1> Hello <?php $_POST['name']?></h1>
     <h1> Your  age is <?php echo $interval->format('%y');?> years and <?php echo $interval->format('%m');?> months </h1>
     </div>
 </body>
